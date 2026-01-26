9d8b60b404fe3a37008232a8325adc26edb5f535
tetey7 <aglagunda@gmail.com>
2026-01-26 18:24:21 +0800

    [#4] Added tasks to django admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "priority", "due_date", "completed", "created_at")
+    list_filter = ("priority", "completed", "created_at", "due_date")
+    search_fields = ("title", "description")
+    list_editable = ("completed",)
+    readonly_fields = ("created_at",)
+    date_hierarchy = "due_date"
+
+
+admin.site.register(Task, TaskAdmin)
